b92bc50aa36321e08ca8374e3c93551747105ca4
Luke <luke@Lifttop.local>
2009-12-23 13:33:26 -0700

    actually fixed the global issue

--- csvimporter/models.py
+++ csvimporter/models.py
@@ -4,8 +4,10 @@ from django.conf import settings
 
 class CSV(models.Model):
     
+    upload_to = getattr(settings, 'CSVIMPORTER_UPLOAD_TO', 'csvimporter')
+    
     content_type = models.ForeignKey(ContentType)
-    csv_file     = models.FileField(upload_to=getattr(settings, 'CSVIMPORTER_UPLOAD_TO', 'csvimporter'))
+    csv_file     = models.FileField(upload_to=upload_to)
     created      = models.DateTimeField(auto_now_add=True)
 
     @property
@@ -14,4 +16,4 @@ class CSV(models.Model):
         This is a helper method, so that you can display the name of the file a user uploaded, without
         the name of the directory the file was uploaded to.
         """
-        return self.csv_file.name.replace('%s/' % upload_to, '')
+        return self.csv_file.name.replace('%s/' % self.upload_to, '')
